3ddfc008f866efa3361a94ff5d602b5b54f3143a
sopheaklaing <sopheaksol71@gmail.com>
2026-01-08 15:31:12 +0700

    vercel home

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
-import Image from "next/image";
-import Login_registration from "./(auth)/login_registration/page";
-export default function App() {
-  return (
-    <div >
-      <main >
-        <Login_registration/>
-      </main>
-    </div>
-  );
+// import Image from "next/image";
+// import Login_registration from "./(auth)/login_registration/page";
+// export default function App() {
+//   return (
+//     <div >
+//       <main >
+//         <Login_registration/>
+//       </main>
+//     </div>
+//   );
+// }
+// src/app/page.js
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/login_registration");
 }
